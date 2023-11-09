604fe49d6cd3d6162e456041608ab3a2bef9b1fc
ckh <gusrudchl12@gmail.com>
2023-11-09 12:07:34 +0900

    11-09

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,20 +6,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        String myString = "ABBAA";
-        String pat = "AABB";
-        myString = myString.replaceAll("A","b");
-        myString = myString.replaceAll("B","a");
-        myString = myString.toUpperCase();
+        List<String> list = new ArrayList<>();
+        String[] todo_list = {"problemsolving", "practiceguitar", "swim", "studygraph"};
+        boolean[] finished = {true, false, true, false};
 
-        if (myString.contains(pat)){
-            System.out.println(1);
-        }else {
-            System.out.println(0);
+        for (int i = 0; i < todo_list.length; i++){
+            if (finished[i] == true){
+                list.add(todo_list[i]);
+            }
         }
-        System.out.println(myString);
-
-
+        System.out.println(list);
     }
 }
 
