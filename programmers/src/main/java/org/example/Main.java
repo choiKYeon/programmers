aa9b19293580db0dddf858930632a801283d3826
ckh <gusrudchl12@gmail.com>
2023-11-10 15:33:11 +0900

    11-10 programmers

--- programmers/src/main/java/org/example/Main.java
+++ programmers/src/main/java/org/example/Main.java
@@ -4,18 +4,14 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {49, 12, 100, 276, 33};
-        int n = 27;
-
-        for (int i = 0; i <= arr.length/2; i++){
-            if (arr.length % 2 == 0){
-                int j = 1;
-                arr[j] = arr[j] + n;
-                j++;
-            } else if (arr.length % 2 == 1) {
-                arr[i*2] = arr[i*2] + n;
-            }
+        int answer = 0;
+        if (a % 2 != 0 && b % 2 != 0){
+            answer = a*a + b*b;
+        } else if (a % 2 != 0 || b % 2 != 0) {
+            answer = 2 * (a + b);
+        } else if () {
+            answer = Math.abs(a - b);
         }
-        System.out.println(arr);
+
     }
 }
