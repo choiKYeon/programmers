26f083ca8c20bb8521d5fb530815e21005b228b7
ckh <gusrudchl12@gmail.com>
2023-11-10 15:24:57 +0900

    11-10

--- programmers/src/main/java/org/example/Main.java
+++ programmers/src/main/java/org/example/Main.java
@@ -1,18 +1,21 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<>();
-        int[] arr = {5, 1, 4};
+        int[] arr = {49, 12, 100, 276, 33};
+        int n = 27;
 
-        for (int i = 0; i <= arr.length; i++){
-            for (int j = 0; j < arr[i]; j++){
-                list.add(arr[i]);
+        for (int i = 0; i <= arr.length/2; i++){
+            if (arr.length % 2 == 0){
+                int j = 1;
+                arr[j] = arr[j] + n;
+                j++;
+            } else if (arr.length % 2 == 1) {
+                arr[i*2] = arr[i*2] + n;
             }
         }
-        System.out.println(list);
+        System.out.println(arr);
     }
 }
