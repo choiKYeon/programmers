9a94151c8d0b93a6e57dea3dcb95716d9f9f86c8
ckh <gusrudchl12@gmail.com>
2023-11-14 12:12:31 +0900

    11-14

--- programmers/src/main/java/org/example/Main.java
+++ programmers/src/main/java/org/example/Main.java
@@ -2,18 +2,30 @@ package org.example;
 
 import java.util.*;
 
-
 public class Main {
     public static void main(String[] args) {
-        String answer = "";
-        String cipher = "dfjardstddetckdaccccdegk";
-        String[] str = cipher.split("");
-        int code = 4;
-        for (int i = 0; i <= cipher.length(); i++){
-            if (i % 4 == 0 && i != 0){
-                answer += str[i - 1];
-            }
+
+
+
+        String s = "3people unFollowed me";
+        String lastAnswer = "";
+        String s1= s.toLowerCase();
+        String[] strList = s1.split(" ");
+        int j = 0;
+        for (int i = 0; i < strList.length; i++){
+            String[] strList2 = strList[i].split("");
+            String s2 = " " + strList2[0].toUpperCase();
+
+            String answer = "";
+                for (int q = 1; q < strList2.length; q++) {
+                    if (q != 0) {
+                        answer += strList2[q];
+                    }
+                }
+            System.out.println(answer);
+            lastAnswer += s2 + answer;
         }
-        System.out.println(answer);
+        System.out.println(lastAnswer.trim());
+
     }
 }
