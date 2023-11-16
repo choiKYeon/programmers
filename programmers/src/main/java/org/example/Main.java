c1754372e5f29ce773624649ce58186080e94e3f
ckh <gusrudchl12@gmail.com>
2023-11-16 11:10:10 +0900

    11-16

--- programmers/src/main/java/org/example/Main.java
+++ programmers/src/main/java/org/example/Main.java
@@ -4,21 +4,28 @@ import java.util.*;
 // 2진수 변환 Integer.toBinaryString
 public class Main {
     public static void main(String[] args) {
-        String s = "cdcd";
-       char[] str = s.toCharArray();
-       Stack<Character> sList = new Stack<>();
+        int brown = 20;
+        int yellow = 16;
+        int result = brown + yellow;
+        int a = 0;
+        int b = 0;
+        int[] answer = {a, b};
 
-       for (int i = 0; i < str.length; i++){
-           char c = str[i];
-           if (sList.empty()) sList.push(c);
-           else {
-               if (sList.peek() == c){
-                   sList.pop();
-               }else {
-                   sList.push(c);
-               }
-           }
-       }
-        System.out.println(sList.empty() ? 1 : 0);
+        for (int i = 1; i < brown/2; i++){
+            for (int j = 1; j <= i; j++){
+                if (result == i * j){
+                    answer[0] = i;
+                    answer[1] = j;
+                    break;
+                }
+            }
+            if (i * i == result){
+                answer[0] = i;
+                answer[1] = i;
+                break;
+            }
+        }
+        System.out.println(answer[0]);
+        System.out.println(answer[1]);
     }
 }
