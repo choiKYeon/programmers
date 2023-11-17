695845d442347fa6d03715f5f4188855ef3c7dc8
ckh <gusrudchl12@gmail.com>
2023-11-17 11:13:58 +0900

    11-17

--- programmers/src/main/java/org/example/Main.java
+++ programmers/src/main/java/org/example/Main.java
@@ -4,25 +4,22 @@ import java.util.*;
 // 2진수 변환 Integer.toBinaryString
 public class Main {
     public static void main(String[] args) {
-        int brown = 20;
-        int yellow = 16;
-        int result = brown + yellow;
-        int a = 0;
-        int b = 0;
-        int[] answer = {a, b};
+        int[] answer = new int[2];
+        String[] words = {"tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"};
+        int n = 3;
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < words.length; i++){
+            if (list.contains(words[i])){
+                answer = new int[]{(i % n) + 1, (int)Math.ceil((i + 1) / n)};
+                break;
+            }
+            list.add(words[i]);
 
-        for (int i = 1; i < brown/2; i++){
-            for (int j = 1; j <= i; j++){
-                if (result == i * j){
-                    answer[0] = i;
-                    answer[1] = j;
+            if (i > 0) {
+                if (words[i - 1].charAt(words[i - 1].length() - 1) != words[i].charAt(0)) {
+                    answer = new int[]{(i % n) + 1, (int) Math.ceil((i + 1) / n)};
                     break;
                 }
-            }
-            if (i * i == result){
-                answer[0] = i;
-                answer[1] = i;
-                break;
             }
         }
         System.out.println(answer[0]);
